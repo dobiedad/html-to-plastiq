914f02f0f00451e8d694c8bec3653dc986f7ed76
Josh Chisholm <joshuachisholm@gmail.com>
2016-05-20 18:44:52 +0100

    rename and move functions for clarity

--- index.js
+++ index.js
@@ -13,18 +13,10 @@ function plastiqifyVNode(vnode) {
     return plastiqifyVNode(vnode[0]);
   }
   if (vnode.tagName) {
-    return { selector: selectorFor(vnode), arguments: childArguments(vnode) };
+    return { selector: selectorFor(vnode), arguments: plastiqHtmlArgumentsFor(vnode) };
   } else {
     return vnode.text;
   }
-}
-
-function childArguments(vnode) {
-  return attributesOf(vnode).concat(removeWhitespaces(childNodesOf(vnode)));
-}
-
-function removeWhitespaces(arguments) {
-  return arguments.filter(function(a) { return !/^\s*$/g.test(a); })
 }
 
 function selectorFor(vnode) {
@@ -33,6 +25,14 @@ function selectorFor(vnode) {
     selector += '#' + vnode.properties.id;
   }
   return selector + classesFor(vnode);
+}
+
+function plastiqHtmlArgumentsFor(vnode) {
+  return attributesOf(vnode).concat(removeWhitespaces(plastiqifyChildrenOf(vnode)));
+}
+
+function removeWhitespaces(arguments) {
+  return arguments.filter(function(a) { return !/^\s*$/g.test(a); })
 }
 
 function classesFor(vnode) {
@@ -64,7 +64,7 @@ function attributesOf(vnode) {
   return [];
 }
 
-function childNodesOf(vnode) {
+function plastiqifyChildrenOf(vnode) {
   if (vnode.children) {
     return vnode.children.map(function(child) {
       return plastiqifyVNode(child);
@@ -74,9 +74,9 @@ function childNodesOf(vnode) {
   }
 }
 
-function generateFunctionFromPlastiqModel(m, indent) {
+function callPlastiqHtml(m, indent) {
   return spaces(indent) + 'h(' + [JSON.stringify(m.selector)].concat((m.arguments || []).map(function(a) {
-    return typeof(a) == 'string' || !a.selector ? JSON.stringify(a) : generateFunctionFromPlastiqModel(a, indent + 1);
+    return typeof(a) == 'string' || !a.selector ? JSON.stringify(a) : callPlastiqHtml(a, indent + 1);
   })).join(", ") + ")";
 }
 
@@ -86,5 +86,5 @@ function spaces(indent) {
 
 function generateRenderFunctionFromVTree(vtree) {
   var m = plastiqifyVNode(vtree);
-  return "function render() {\n  return " + generateFunctionFromPlastiqModel(m, 2).substr(3) + "\n}";
+  return "function render() {\n  return " + callPlastiqHtml(m, 2).substr(3) + "\n}";
 }
